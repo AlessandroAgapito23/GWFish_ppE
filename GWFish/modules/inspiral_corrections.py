6eb1022a2e28f75398f7310a38869416e31f2326
Alessandro Agapito <160891152+AlessandroAgapito23@users.noreply.github.com>
2024-03-15 14:49:06 +0100

    Update inspiral_corrections.py

--- GWFish/modules/inspiral_corrections.py
+++ GWFish/modules/inspiral_corrections.py
@@ -219,6 +219,7 @@ class TaylorF2_PPE(Waveform):
         
     def plot (self, output_folder='./'):
 
+        M, mu, Mc, delta_mass, eta, eta2, eta3, chi_eff, chi_PN, chi_s, chi_a, C, ff = wf.Waveform.get_param_comb(self)
         psi_TF2, psi_TF2_prime, psi_TF2_f1, psi_TF2_prime_f1 = wf.TaylorF2.calculate_phase(self)
         psi, psi_prime, psi_f1, psi_prime_f1 = TaylorF2_PPE.calculate_phase(self)
         
@@ -241,7 +242,7 @@ class TaylorF2_PPE(Waveform):
 
 
         plt.figure()
-        plt.semilogx(self.frequencyvector, phase)
+        plt.semilogx(ff, phase)
         plt.xlabel('Frequency [Hz]')
         plt.ylabel('Phase [rad]')
         plt.grid(which='both', color='lightgray', alpha=0.5, linestyle='dashed', linewidth=0.5)
@@ -251,7 +252,7 @@ class TaylorF2_PPE(Waveform):
 
         
         plt.figure()
-        plt.semilogx(self.frequencyvector, delta_phase)
+        plt.semilogx(ff, delta_phase)
         plt.xlabel('Frequency [Hz]')
         plt.ylabel('Phase difference [rad]')
         plt.grid(which='both', color='lightgray', alpha=0.5, linestyle='dashed', linewidth=0.5)
