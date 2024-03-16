a7a0f2cd740a26374b40acaeaeb2a6bd276091a5
Alessandro Agapito <160891152+AlessandroAgapito23@users.noreply.github.com>
2024-03-16 18:54:17 +0100

    Update inspiral_corrections.py

--- GWFish/modules/inspiral_corrections.py
+++ GWFish/modules/inspiral_corrections.py
@@ -67,33 +67,7 @@ class TaylorF2_PPE(Waveform):
         return PN, beta, delta_phi_0, delta_phi_1, delta_phi_2, delta_phi_3, delta_phi_4,\
         delta_phi_5, delta_phi_6, delta_phi_7, delta_phi_8, delta_phi_9
 
-
-    def EI_phase_coeff(self):
-
-        M, mu, Mc, delta_mass, eta, eta2, eta3, chi_eff, chi_PN, chi_s, chi_a, C, ff = wf.Waveform.get_param_comb(self)
     
-        # Coefficienti della fase dell'ispirazione precoce
-        phi_0 = 1.
-        phi_1 = 0.
-        phi_2 = 3715./756. + 55./9.*eta
-        phi_3 = -16.*np.pi + 113./3.*delta_mass*chi_a + (113./3. - 76./3.*eta)*chi_s
-        phi_4 = 15293365./508032. + 27145./504.*eta + 3085./72.*eta2 + (-(405./8.) +\
-                200*eta)*chi_a**2 - 405./4.*delta_mass*chi_a*chi_s + (-(405./8.) + 5./2.*eta)*chi_s**2
-        phi_5 = 38645./756.*np.pi - 65./9.*np.pi*eta + delta_mass*(-(732985./2268.) -\
-                140./9.*eta)*chi_a + (-(732985./2268.) + 24260./81.*eta + 340./9.*eta2)*chi_s
-        phi_5_l = 3*phi_5
-        phi_6 = 11583231236531./4694215680. - 6848./21.*C - (640.*np.pi**2)/3. +\
-            (-15737765635./3048192. + 2255.*np.pi**2/12.)*eta + 76055.*eta2/1728. - 127825.*eta3/1296. +\
-            2270./3.*np.pi*delta_mass*chi_a + (2270.*np.pi/3. - 520.*np.pi*eta)*chi_s -6848./63.*np.log(64.)
-        phi_6_l = (-6848./63.)*3
-        phi_7 = (77096675./254016. + 378515./1512.*eta - 74045./756.*eta2)*np.pi +\
-            delta_mass*(-(25150083775./3048192.) + 26804935./6048.*eta - 1985./48.*eta2)*chi_a +\
-            (-(25150083775./3048192.) + 10566655595./762048.*eta - 1042165./3024.*eta2 + 5345./36.*eta3)*chi_s
-    
-        return phi_0, phi_1, phi_2, phi_3, phi_4, phi_5, phi_5_l, phi_6, phi_6_l, phi_7
-
-    
-        
     def calculate_phase(self): 
 
         M, mu, Mc, delta_mass, eta, eta2, eta3, chi_eff, chi_PN, chi_s, chi_a, C, ff = wf.Waveform.get_param_comb(self)
@@ -119,7 +93,7 @@ class TaylorF2_PPE(Waveform):
 
         psi_TF2, psi_TF2_prime, psi_TF2_f1, psi_TF2_prime_f1 = wf.TaylorF2.calculate_phase(self)
 
-        phi_0, phi_1, phi_2, phi_3, phi_4, phi_5, phi_5_l, phi_6, phi_6_l, phi_7 = TaylorF2_PPE.EI_phase_coeff(self)
+        phi_0, phi_1, phi_2, phi_3, phi_4, phi_5, phi_5_l, phi_6, phi_6_l, phi_7 = wf.TaylorF2.EI_phase_coeff(self)
 
         psi_gIMR = 3./(128.*eta)*(delta_phi_0*(np.pi*ff)**(-5./3.) +\
                 delta_phi_1*(np.pi*ff)**(-4./3.)+\
@@ -261,8 +235,6 @@ class TaylorF2_PPE(Waveform):
 
 
 ################################################################################
-
-################################################################################
 ############################## IMRPhenomD_PPE ##################################
 ################################################################################
 
@@ -277,74 +249,6 @@ class IMRPhenomD_PPE(Waveform):
             logging.warning('Different waveform name passed to IMRPhenomD_PPE: '+\
                              self.name)
 
-    # Here we want to define the late-inspiral, intermediate & merger-ringdown phase coefficients
-
-    def LI_phase_coeff(self):
-    
-        M, mu, Mc, delta_mass, eta, eta2, eta3, chi_eff, chi_PN, chi_s, chi_a, C, ff = wf.Waveform.get_param_comb(self)
-    
-        #LATE INSPIRAL Phase Coefficients >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-        #(sigma0=sigma1=0 due to phase translation)
-        
-        sigma2 = -10114.056472621156 - 44631.01109458185*eta\
-                + (chi_PN - 1)*(-6541.308761668722 - 266959.23419307504*eta + 686328.3229317984*eta2)\
-                + (chi_PN - 1)**2*(3405.6372187679685 - 437507.7208209015*eta + 1.6318171307344697e6*eta2)\
-                + (chi_PN - 1)**3*(-7462.648563007646 - 114585.25177153319*eta + 674402.4689098676*eta2)
-        sigma3 = 22933.658273436497 + 230960.00814979506*eta\
-                + (chi_PN - 1)*(14961.083974183695 + 1.1940181342318142e6*eta - 3.1042239693052764e6*eta2)\
-                + (chi_PN - 1)**2*(-3038.166617199259 + 1.8720322849093592e6*eta - 7.309145012085539e6*eta2)\
-                + (chi_PN - 1)**3*(42738.22871475411 + 467502.018616601*eta - 3.064853498512499e6*eta2)
-        sigma4 = -14621.71522218357 - 377812.8579387104*eta\
-                + (chi_PN - 1)*(-9608.682631509726 - 1.7108925257214056e6*eta + 4.332924601416521e6*eta2)\
-                + (chi_PN - 1)**2*(-22366.683262266528 - 2.5019716386377467e6*eta + 1.0274495902259542e7*eta2)\
-                + (chi_PN - 1)**3*(-85360.30079034246 - 570025.3441737515*eta + 4.396844346849777e6*eta2)
-
-        return sigma2, sigma3, sigma4
-
-    def INT_phase_coeff(self):
-
-        M, mu, Mc, delta_mass, eta, eta2, eta3, chi_eff, chi_PN, chi_s, chi_a, C, ff = wf.Waveform.get_param_comb(self)
-
-        # PN coefficients for the INTERMEDIATE PHASE >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-        #beta0 and beta1 are fixed by the continuity conditions 
-        
-        beta2 = -3.282701958759534 - 9.051384468245866*eta\
-                + (chi_PN - 1)*(-12.415449742258042 + 55.4716447709787*eta - 106.05109938966335*eta2)\
-                + (chi_PN - 1)**2*(-11.953044553690658 + 76.80704618365418*eta - 155.33172948098394*eta2)\
-                + (chi_PN - 1)**3*(-3.4129261592393263 + 25.572377569952536*eta - 54.408036707740465*eta2)
-        beta3 = -0.000025156429818799565 + 0.000019750256942201327*eta\
-                + (chi_PN - 1)**2*(-0.000018370671469295915 + 0.000021886317041311973*eta + 0.00008250240316860033*eta2)\
-                + (chi_PN - 1)**2*(7.157371250566708e-6 - 0.000055780000112270685*eta + 0.00019142082884072178*eta2)\
-                + (chi_PN - 1)**3*(5.447166261464217e-6 - 0.00003220610095021982*eta + 0.00007974016714984341*eta2)
-    
-        return beta2, beta3
-
-    def MR_phase_coeff(self):
-
-        M, mu, Mc, delta_mass, eta, eta2, eta3, chi_eff, chi_PN, chi_s, chi_a, C, ff = wf.Waveform.get_param_comb(self)
-
-        # PN coefficients for the MERGER-RINGDOWN PHASE>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-        #alpha0 and alpha1 are fixed by the continuity conditions
-        
-        alpha2 = -0.07020209449091723 - 0.16269798450687084*eta\
-                + (chi_PN - 1)*(-0.1872514685185499 + 1.138313650449945*eta - 2.8334196304430046*eta2)\
-                + (chi_PN - 1)**2*(-0.17137955686840617 + 1.7197549338119527*eta - 4.539717148261272*eta2)\
-                + (chi_PN - 1)**3*(-0.049983437357548705 + 0.6062072055948309*eta - 1.682769616644546*eta2)
-        alpha3 = 9.5988072383479 - 397.05438595557433*eta\
-                + (chi_PN - 1)*(16.202126189517813 - 1574.8286986717037*eta + 3600.3410843831093*eta2)\
-                + (chi_PN - 1)**2*(27.092429659075467 - 1786.482357315139*eta + 5152.919378666511*eta2)\
-                + (chi_PN - 1)**3*(11.175710130033895 - 577.7999423177481*eta + 1808.730762932043*eta2)
-        alpha4 =  -0.02989487384493607 + 1.4022106448583738*eta\
-                + (chi_PN - 1)*(-0.07356049468633846 + 0.8337006542278661*eta + 0.2240008282397391*eta2)\
-                + (chi_PN - 1)**2*(-0.055202870001177226 + 0.5667186343606578*eta + 0.7186931973380503*eta2)\
-                + (chi_PN - 1)**3*(-0.015507437354325743 + 0.15750322779277187*eta + 0.21076815715176228*eta2)
-        alpha5 = 0.9974408278363099 - 0.007884449714907203*eta\
-                + (chi_PN - 1)*(-0.059046901195591035 + 1.3958712396764088*eta - 4.516631601676276*eta2)\
-                + (chi_PN - 1)**2*(-0.05585343136869692 + 1.7516580039343603*eta - 5.990208965347804*eta2)\
-                + (chi_PN - 1)**3*(-0.017945336522161195 + 0.5965097794825992*eta - 2.0608879367971804*eta2)
-    
-        return alpha2, alpha3, alpha4, alpha5
-
     # Here we add the phase deviations, which satisfy the continuity conditions of the phase and its derivative at the inferface
 
     def calculate_phase(self): 
@@ -358,7 +262,7 @@ class IMRPhenomD_PPE(Waveform):
         PN, beta, delta_phi_0, delta_phi_1, delta_phi_2, delta_phi_3, delta_phi_4,\
         delta_phi_5, delta_phi_6, delta_phi_7, delta_phi_8, delta_phi_9 = TaylorF2_PPE.get_phase_corr(self)
         
-        phi_0, phi_1, phi_2, phi_3, phi_4, phi_5, phi_5_l, phi_6, phi_6_l, phi_7 = TaylorF2_PPE.EI_phase_coeff(self)
+        phi_0, phi_1, phi_2, phi_3, phi_4, phi_5, phi_5_l, phi_6, phi_6_l, phi_7 = wf.TaylorF2.EI_phase_coeff(self)
 
         psi_TF2, psi_TF2_prime, psi_TF2_f1, psi_TF2_prime_f1 = wf.TaylorF2.calculate_phase(self)
 
@@ -375,7 +279,7 @@ class IMRPhenomD_PPE(Waveform):
 
         psi = psi_TF2 + psi_ppe + psi_gIMR
 
-        sigma2, sigma3, sigma4 = IMRPhenomD_PPE.LI_phase_coeff(self)
+        sigma2, sigma3, sigma4 = wf.IMRPhenomD.LI_phase_coeff(self)
 
         psi_late_ins = + 1./eta*(3./4.*sigma2*ff**(4./3.) +\
                             3./5.*sigma3*ff**(5./3.) +\
@@ -431,7 +335,7 @@ class IMRPhenomD_PPE(Waveform):
         ####################### INS-INT PHASE CONTINUITY CONDITIONS ###################
         # Impose C1 conditions at the interface (same conditions as in IMRPhenomD but with different psi_ins & psi_ins_prime)
 
-        beta2, beta3 = IMRPhenomD_PPE.INT_phase_coeff(self)
+        beta2, beta3 = wf.IMRPhenomD.INT_phase_coeff(self)
 
         beta1 = eta*psi_ins_prime_f1 - beta2*f1**(-1.) - beta3*f1**(-4.)  # psi_ins_prime_f1 = psi_int_prime_f1
         beta0 = eta*psi_ins_f1 - beta1*f1 - beta2*np.log(f1) + beta3/3.*f1**(-3.) #psi_ins_f1 = psi_int_f1
@@ -449,7 +353,7 @@ class IMRPhenomD_PPE(Waveform):
         
         ####################### IN-MERG PHASE CONTINUITY CONDITIONS ###################
 
-        alpha2, alpha3, alpha4, alpha5 = IMRPhenomD_PPE.MR_phase_coeff(self)
+        alpha2, alpha3, alpha4, alpha5 = wf.IMRPhenomD.MR_phase_coeff(self)
         
         alpha1 = psi_int_prime_f2 - alpha2*f2**(-2.) - alpha3*f2**(-1./4.) -\
                 (alpha4*ff_damp)/(ff_damp**2. + (f2 - alpha5*ff_RD)**2.) # psi_int_prime_f2 = psi_MR_prime_f2
@@ -485,7 +389,6 @@ class IMRPhenomD_PPE(Waveform):
         return psi_tot
 
         
-
     def calculate_frequency_domain_strain(self): 
 
         psi = IMRPhenomD_PPE.calculate_phase(self)
