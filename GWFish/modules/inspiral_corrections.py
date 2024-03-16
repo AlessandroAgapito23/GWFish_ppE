0ed68fea36e076c6b8c79e5234c766c423822fb0
Alessandro Agapito <160891152+AlessandroAgapito23@users.noreply.github.com>
2024-03-16 21:03:07 +0100

    Update inspiral_corrections.py

--- GWFish/modules/inspiral_corrections.py
+++ GWFish/modules/inspiral_corrections.py
@@ -173,7 +173,7 @@ class TaylorF2_PPE(Inspiral_corr):
                                              np.log(np.pi*f1)*(np.pi)**(1./3.)*(1./3.*f1**(-2./3.))) +\
                         phi_7*delta_phi_7*(np.pi)**(2./3.)*(2./3.*f1**(-1./3.)))
 
-        psi_ppe_prime = beta*(2*PN-5.)/3.*((np.pi*(ff*cst.c**3/(cst.G*M)*Mc)**((2*PN-8.)/3.))
+        psi_ppe_prime = beta*(2*PN-5.)/3.*(np.pi* ff * cst.c**3 / (cst.G*M) *Mc)**((2*PN-8.)/3.)
                                            
         psi_ppe_prime_f1 = beta*(2*PN-5.)/3.*((np.pi*(f1*cst.c**3/(cst.G*M)*Mc)**((2*PN-8.)/3.))
         
