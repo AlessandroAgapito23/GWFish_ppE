e84749c40ff41855af09b846db482f03e0e7cab3
Alessandro Agapito <160891152+AlessandroAgapito23@users.noreply.github.com>
2024-03-27 17:53:24 +0100

    Update inspiral_corrections.py

--- GWFish/modules/inspiral_corrections.py
+++ GWFish/modules/inspiral_corrections.py
@@ -358,6 +358,8 @@ class TaylorF2_mult(Inspiral_corr):
         phi_0, phi_1, phi_2, phi_3, phi_4, phi_5, phi_5_l, phi_6, phi_6_l, phi_7 = wf.TaylorF2.EI_phase_coeff(self)
         P4, P6, P7, P8 = TaylorF2_mult.INS_mult_coeff(self)
 
+
+        P8 = 0.
         psi_mult = 3./(128.*eta)*(P4*(np.pi*ff)**(-1./3.) +\
                                   P6*(np.pi*ff)**(1./3.) +\
                                   P7*(np.pi*ff)**(2./3.) +\
@@ -411,7 +413,7 @@ class TaylorF2_mult(Inspiral_corr):
                  a_3*(np.pi*ff) +\
                  a_4*(np.pi*ff)**(4./3.)
          
-        amp_tot = A0
+        amp_tot = amp_PN*A0
          
         hp = amp_tot*0.5*(1 + np.cos(iota)**2.)
         hc = amp_tot*np.cos(iota)
